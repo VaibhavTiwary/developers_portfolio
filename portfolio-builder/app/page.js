2967fb864c726b62ad9bd8d9114c7a681cb4a864
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-07-05 14:12:02 +0530

    change authenticatio  path to localhost

--- portfolio-builder/app/page.js
+++ portfolio-builder/app/page.js
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       hello from next
+      <label>hii</label>
+      <UserButton />
     </div>
   );
 }
